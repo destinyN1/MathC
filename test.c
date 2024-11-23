c2e253384be03a3ce4a590748363c2d033e21788
DestinyN1 <d.newman7@universityofgalway.ie>
2024-11-23 19:28:04 +0000

    cosine function printing correctly

--- test.c
+++ test.c
@@ -3,54 +3,92 @@
 #include <stdlib.h>
 
 
-
-
 //interval
 int a; 
 int b;
 
-int cosine();
-int sinusoid();
+//selector
+int slct;
+
+void cosine(int *prt);
+//void sinusoid(int *prt);
+int FillInt(int *ptr);
 
 int main(){
+ int *ptr;
+
 // inputs
   printf("Define the start of the interval \n");
   scanf("%d", &a);
   printf("Define the end of the interval \n");
   scanf("%d",&b);
+  printf("What would ou like to print\n1. Cosine Function\n2. Sine Function");
+  scanf("%d",&slct);
 
 
-//Defining a null pointer for dynamic array
-int *ptr = (int*)malloc(((b+1)-a) * sizeof(int)); //size of int is 4bytes
+  //allocating a memory block for ptr
+  ptr = (int*)malloc(((b+1)-a) * sizeof(int));
+  if (ptr == NULL){
+    printf("memory not allocated properly\n");
+    return 1;
+}
+  FillInt(ptr);
+
+switch (slct)
+{
+case 1:
+  cosine(ptr);
+  break;
+//\case 2:
+ //sinusoid(ptr);
+ //break;
+
+default:
+printf("Invalid selection");
+break;
+free(ptr);
+return 0; 
+
+}
+
+}
+
+//passing the address block so that it can modify it
+int FillInt(int *ptr){
 
 //checking proper memory allocation
-if (ptr == NULL){
-    printf("memory not allocated properly\n");
-}
-else{
-  int k = 0;
  printf("memory allocated \n");
+ int k = 0;
  for(int i = a; i<=b; ++i){
+  
     ptr[k] = i;
     k++;
  }
- //printing for tests
+ //printing X values
  for(int i = 0; i<=(b-a); ++i){
-  printf("ptr[%d] = %d\n", i, ptr[i]);
+
+//shallow copy of the pointer
+ int *x = ptr;  
+
+
+  printf("x[%d] = %d\n", i, x[i]);
  }  
-    
-}
+   return 0;
  
-int cosine(){
-  for(){
+}
 
+
+ 
+void cosine(int *ptr){
+  for(int i = 0; i<=(b-a); ++i){
+   printf("cos(%d) = %.2f\n", ptr[i], cos(ptr[i]));
+  
   }
-  return 0;
 }
 
  
 
 
-free(ptr); //freeing allocated memory    
-return 0;
-}
+
+
+
